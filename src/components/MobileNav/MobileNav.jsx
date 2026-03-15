3c55746519c2af51ffe7b58416e3f2b3ad518727
Rich <TonyRiches17@yahoo.com>
2026-03-15 12:31:37 -0400

    Fix MobileNav

--- src/components/MobileNav/MobileNav.jsx
+++ src/components/MobileNav/MobileNav.jsx
@@ -6,7 +6,8 @@ import mobilenav from "../../assets/mobilenav.jpg";
 function MobileNav({ isScrolled }) {
 const [buttonClicked, setButtonClicked] = useState(false);
 
-const handleButtonClickToggle = () => {
+const handleButtonClickToggle = (evt) => {
+  evt.stopPropagation();
   setButtonClicked(!buttonClicked);
 }
 
@@ -14,13 +15,16 @@ const handleButtonClickToggle = () => {
   return(
     <div className={isScrolled? "mobilenav" : "mobilenav_off"}>
       <Overlay buttonClicked={buttonClicked} />
-      <button onClick={handleButtonClickToggle} className="mobilenav__button">
+      <div className="mobilenav__border-hide">
+      <button onClick={handleButtonClickToggle} className={buttonClicked ? "mobilenav__button_clicked" : "mobilenav__button"}>
         <img src={mobilenav} alt="Picture of a coin on the Mobil Navigation button" className={buttonClicked ? "mobilenav__button-image_clicked" : "mobilenav__button-image"} />
       </button>
-      <div className="mobilenav__links">
-        <p className="mobilenav__link">Biography</p>
-        <p className="mobilenav__link">Portfolio</p>
-        <p className="mobilenav__link">Contact Me</p>
+      <div className={buttonClicked ? "mobilenav__links_clicked" : "mobilenav__links"}>
+        <p className="mobilnav__link"></p>
+        <p onClick={() => {window.location.href="#bio"; setButtonClicked(false);}} className="mobilenav__link">Biography</p>
+        <p onClick={() => {window.location.href="#portfolio"; setButtonClicked(false);}} className="mobilenav__link">Portfolio</p>
+        <p onClick={() => {window.location.href="#contact"; setButtonClicked(false);}} className="mobilenav__link">Contact Me</p>
+      </div>
       </div>
     </div>
   )
